0f1de199ff6004c6266d03f77036805eecb4cbee
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-02-05 17:07:57 +0100

    Syntax idea to make it simpler

--- examples/last-10-builds-async.php
+++ examples/last-10-builds-async.php
@@ -9,10 +9,10 @@ require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 $client = new Client();
 $travis = new Travis($client);
 
-$client->requestAsync($travis->repository('WyriHaximus/php-travis-client')->builds())->then(function ($builds) {
+$travis->repository('WyriHaximus/php-travis-client')->builds()->then(function ($builds) {
     foreach ($builds as $build) {
         echo 'Build: #', $build->getId(), PHP_EOL;
-        /*$build->getClient()->requestAsync($build->matrix())->then(function ($matrix) {
+        /*$build->matrix()->then(function ($matrix) {
             echo 'Build: #', $matrix->getBuild()->getId(), PHP_EOL;
             foreach ($matrix as $matri) {
                 echo "\t", 'php: ', $matri->getPHP(), PHP_EOL;
