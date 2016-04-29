48b7ba5b06cbaab0843585ed4da315d7d3d7bcad
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-04-29 07:44:18 +0200

    Smoother observables implementation by @mbonneau from https://gist.github.com/mbonneau/6900873fff836f9d30759ace30ab9bc1

--- src/Resource/Async/Repository.php
+++ src/Resource/Async/Repository.php
@@ -6,6 +6,7 @@ namespace WyriHaximus\Travis\Resource\Async;
 use Rx\Observable;
 use Rx\ObservableInterface;
 use Rx\ObserverInterface;
+use Rx\React\Promise;
 use WyriHaximus\Travis\Resource\Async\Build;
 use WyriHaximus\Travis\Resource\Repository as BaseRepository;
 use function React\Promise\resolve;
@@ -14,12 +15,12 @@ class Repository extends BaseRepository
 {
     public function builds(): ObservableInterface
     {
-        return Observable::create(function (ObserverInterface $observer) {
-            $this->getTransport()->request('repos/' . $this->slug() . '/builds')->then(function ($response) use ($observer) {
-                foreach ($response['builds'] as $build) {
-                    $observer->onNext($this->getTransport()->hydrate(Build::class, $build));
-                }
-            });
+        return Promise::toObservable(
+            $this->getTransport()->request('repos/' . $this->slug() . '/builds')
+        )->flatMap(function ($response) {
+            return Observable::fromArray($response['builds']);
+        })->map(function ($build) {
+            return $this->getTransport()->hydrate(Build::class, $build);
         });
     }
 }
