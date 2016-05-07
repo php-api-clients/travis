636bd053d8db9db1f5c6433696f6d70eea4a63ab
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-05-07 02:10:23 +0200

    Build to get specific build

--- src/Resource/Sync/Repository.php
+++ src/Resource/Sync/Repository.php
@@ -5,22 +5,26 @@ namespace WyriHaximus\Travis\Resource\Sync;
 
 use Rx\Observable;
 use Rx\React\Promise;
-use WyriHaximus\Travis\Resource\Async\Repository as AsyncRepository;
 use WyriHaximus\Travis\Resource\Repository as BaseRepository;
 use function Clue\React\Block\await;
 use function React\Promise\resolve;
 
 class Repository extends BaseRepository
 {
-    public function builds()
+    public function builds(): array
     {
         return await(
             Promise::fromObservable(
-                $this->getTransport()->hydrateFQCN(
-                    AsyncRepository::class,
-                    $this->getTransport()->extractFQCN(AsyncRepository::class, $this)
-                )->builds()->toArray()
+                $this->getTransport()->buildAsyncFromSync('Repository', $this)->builds()->toArray()
             ),
+            $this->getTransport()->getLoop()
+        );
+    }
+
+    public function build(int $id): Build
+    {
+        return await(
+            $this->getTransport()->buildAsyncFromSync('Repository', $this)->build($id),
             $this->getTransport()->getLoop()
         );
     }
