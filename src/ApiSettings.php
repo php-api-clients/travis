b884e8a0e253513b4bd60c50db6bc94db4371385
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-05-22 00:24:06 +0200

    Basic repo watch implementation

--- src/ApiSettings.php
+++ src/ApiSettings.php
@@ -5,6 +5,11 @@ namespace WyriHaximus\Travis;
 
 class ApiSettings
 {
+    /**
+     * Travis' Pusher App ID as found on: https://docs.travis-ci.com/api?http#external-apis
+     */
+    const PUSHER_KEY = '5df8ac576dcccf4fd076';
+
     const TRANSPORT_OPTIONS = [
         'host' => 'api.travis-ci.org',
         'namespace' => 'WyriHaximus\\Travis\\Resource',
