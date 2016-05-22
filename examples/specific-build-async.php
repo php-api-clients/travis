4a240fac25778aa2b9f94f6c8c266fc70f4fd49d
David Dan <davidwdan@gmail.com>
2016-05-22 16:21:15 -0400

    Make things more RXy

--- examples/specific-build-async.php
+++ examples/specific-build-async.php
@@ -1,21 +1,21 @@
 <?php
 
 use WyriHaximus\Travis\AsyncClient;
+use WyriHaximus\Travis\Resource\Async\Repository;
 use WyriHaximus\Travis\Resource\BuildInterface;
-use WyriHaximus\Travis\Resource\RepositoryInterface;
 
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 
 $loop = \React\EventLoop\Factory::create();
 $client = new AsyncClient($loop);
 
-$client->repository($argv[1] ?? 'WyriHaximus/php-travis-client')->then(function (RepositoryInterface $repository) use ($argv) {
+$client->repository($argv[1] ?? 'WyriHaximus/php-travis-client')->subscribeCallback(function (Repository $repository) use ($argv) {
     echo 'Repository: ', PHP_EOL;
     echo 'id: ' . $repository->id(), PHP_EOL;
     echo 'slug: ' . $repository->slug(), PHP_EOL;
     echo 'description: ' . $repository->description(), PHP_EOL;
     echo 'Builds:', PHP_EOL;
-    $repository->build($argv[2] ?? 126863927)->then(function (BuildInterface $build) {
+    $repository->build($argv[2] ?? 126863927)->subscribeCallback(function (BuildInterface $build) {
         echo "\t", 'Build', PHP_EOL;
         echo "\t\t" . 'id: ' . $build->id(), PHP_EOL;
         echo "\t\t" . 'commit id: ' . $build->commitId(), PHP_EOL;
