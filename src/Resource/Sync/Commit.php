627bf131eeab3b81f28192fadcf4758f638c971e
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-05-23 17:26:37 +0200

    Missing trait for methods used

--- src/Resource/Sync/Commit.php
+++ src/Resource/Sync/Commit.php
@@ -3,10 +3,13 @@ declare(strict_types=1);
 
 namespace WyriHaximus\Travis\Resource\Sync;
 
+use WyriHaximus\ApiClient\Resource\CallAsyncTrait;
 use WyriHaximus\Travis\Resource\Commit as BaseCommit;
 
 class Commit extends BaseCommit
 {
+    use CallAsyncTrait;
+
     public function refresh(): Commit
     {
         return $this->wait($this->callAsync('refresh'));
--- src/Resource/Sync/Job.php
+++ src/Resource/Sync/Job.php
@@ -3,10 +3,13 @@ declare(strict_types=1);
 
 namespace WyriHaximus\Travis\Resource\Sync;
 
+use WyriHaximus\ApiClient\Resource\CallAsyncTrait;
 use WyriHaximus\Travis\Resource\Job as BaseJob;
 
 class Job extends BaseJob
 {
+    use CallAsyncTrait;
+
     public function refresh(): Job
     {
         return $this->wait($this->callAsync('refresh'));
