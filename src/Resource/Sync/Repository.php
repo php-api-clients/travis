b7da8a3b77d9cd27e6a5a6f7900d4332e9ece468
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-07-02 01:57:41 +0200

    Async to sync conversion

--- src/Resource/Sync/Repository.php
+++ src/Resource/Sync/Repository.php
@@ -26,4 +26,9 @@ class Repository extends BaseRepository
     {
         return $this->wait($this->observableToPromise($this->callAsync('commits')->toArray()));
     }
+
+    public function caches(): array
+    {
+        return $this->wait($this->observableToPromise($this->callAsync('caches')->toArray()));
+    }
 }
