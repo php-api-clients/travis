b9f347315df904853608d2f414d86a5729fb54c7
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-07-02 02:52:06 +0200

    Removed old user statement

--- examples/user-async.php
+++ examples/user-async.php
@@ -2,7 +2,6 @@
 
 use React\EventLoop\Factory;
 use WyriHaximus\Travis\AsyncClient;
-use WyriHaximus\Travis\Resource\RepositoryInterface;
 use WyriHaximus\Travis\Resource\UserInterface;
 
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
