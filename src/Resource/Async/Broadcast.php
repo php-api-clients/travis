9349f06d8a1e2207edeff1ef946e28f182bc4d33
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-07-02 02:13:26 +0200

    Broadcasts can't really be refreshed so we're returning the same object

--- src/Resource/Async/Broadcast.php
+++ src/Resource/Async/Broadcast.php
@@ -3,12 +3,14 @@ declare(strict_types=1);
 
 namespace WyriHaximus\Travis\Resource\Async;
 
+use React\Promise\PromiseInterface;
 use WyriHaximus\Travis\Resource\Broadcast as BaseBroadcast;
+use function React\Promise\resolve;
 
 class Broadcast extends BaseBroadcast
 {
-    public function refresh() : Broadcast
+    public function refresh() : PromiseInterface
     {
-        return $this->wait($this->callAsync('refresh'));
+        return resolve($this);
     }
 }
