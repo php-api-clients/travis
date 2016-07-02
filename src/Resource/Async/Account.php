87783d2f8cb3c482011389fb96baf72918c1f791
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-07-02 23:55:28 +0200

    Returning the correct type (hint)

--- src/Resource/Async/Account.php
+++ src/Resource/Async/Account.php
@@ -3,14 +3,15 @@ declare(strict_types=1);
 
 namespace WyriHaximus\Travis\Resource\Async;
 
+use React\Promise\PromiseInterface;
 use WyriHaximus\Travis\Resource\Account as BaseAccount;
 
 class Account extends BaseAccount
 {
     /**
-     * @return Account
+     * @return PromiseInterface
      */
-    public function refresh() : Account
+    public function refresh() : PromiseInterface
     {
         return $this->wait($this->callAsync('refresh'));
     }
