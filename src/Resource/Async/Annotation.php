8b2f31fdfecd13410ce4a29be7e3e6f13512da4c
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-07-02 23:55:07 +0200

    AsyncAnnotation::refresh docblock

--- src/Resource/Async/Annotation.php
+++ src/Resource/Async/Annotation.php
@@ -3,14 +3,15 @@ declare(strict_types=1);
 
 namespace WyriHaximus\Travis\Resource\Async;
 
+use React\Promise\PromiseInterface;
 use WyriHaximus\Travis\Resource\Annotation as BaseAnnotation;
 
 class Annotation extends BaseAnnotation
 {
     /**
-     * @return Annotation
+     * @return PromiseInterface
      */
-    public function refresh() : Annotation
+    public function refresh() : PromiseInterface
     {
         return $this->wait($this->callAsync('refresh'));
     }
