3eba24dbc5e8f19ac88b6ff4915bb0aff8d7544d
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-07-02 14:25:25 +0200

    Async sync request

--- src/Resource/Async/User.php
+++ src/Resource/Async/User.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace WyriHaximus\Travis\Resource\Async;
 
+use GuzzleHttp\Psr7\Request;
+use React\Promise\PromiseInterface;
 use WyriHaximus\Travis\Resource\User as BaseUser;
 
 class User extends BaseUser
@@ -10,5 +12,18 @@ class User extends BaseUser
     public function refresh() : User
     {
         return $this->wait($this->callAsync('refresh'));
+    }
+
+    public function sync(): PromiseInterface
+    {
+        return $this->getTransport()->requestPsr7(
+            new Request(
+                'POST',
+                $this->getTransport()->getBaseURL() . 'users/sync',
+                $this->getTransport()->getHeaders()
+            )
+        )->then(function () {
+            return $this->refresh();
+        });
     }
 }
