9666b78a53324466d052e958a59e74ad46a58236
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-07-02 23:56:39 +0200

    AsyncBranch::refresh docblock

--- src/Resource/Sync/Branch.php
+++ src/Resource/Sync/Branch.php
@@ -7,6 +7,9 @@ use WyriHaximus\Travis\Resource\Branch as BaseBranch;
 
 class Branch extends BaseBranch
 {
+    /**
+     * @return Branch
+     */
     public function refresh() : Branch
     {
         return $this->wait($this->callAsync('refresh'));
