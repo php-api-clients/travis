ad6e25d97dbceaebd45590f61384832b1e447f58
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-07-02 23:55:47 +0200

    AsyncBranch::refresh docblock

--- src/Resource/Async/Branch.php
+++ src/Resource/Async/Branch.php
@@ -3,11 +3,15 @@ declare(strict_types=1);
 
 namespace WyriHaximus\Travis\Resource\Async;
 
+use React\Promise\PromiseInterface;
 use WyriHaximus\Travis\Resource\Branch as BaseBranch;
 
 class Branch extends BaseBranch
 {
-    public function refresh() : Branch
+    /**
+     * @return PromiseInterface
+     */
+    public function refresh() : PromiseInterface
     {
         return $this->wait($this->callAsync('refresh'));
     }
