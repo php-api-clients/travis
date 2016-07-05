5256dbb72cb043298f04f676cedc3222b7f33415
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-07-05 08:03:40 +0200

    Ordering of methods

--- src/Resource/Sync/User.php
+++ src/Resource/Sync/User.php
@@ -7,13 +7,13 @@ use WyriHaximus\Travis\Resource\User as BaseUser;
 
 class User extends BaseUser
 {
-    public function refresh() : User
-    {
-        return $this->wait($this->callAsync('refresh'));
-    }
-
     public function sync() : User
     {
         return $this->wait($this->callAsync('sync'));
+    }
+
+    public function refresh() : User
+    {
+        return $this->wait($this->callAsync('refresh'));
     }
 }
