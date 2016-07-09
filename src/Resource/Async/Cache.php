a04f289cf0f51ce85d1092c1bb673b7424321540
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-07-09 14:27:03 +0200

    Cache refresh

--- src/Resource/Async/Cache.php
+++ src/Resource/Async/Cache.php
@@ -4,11 +4,23 @@ declare(strict_types=1);
 namespace WyriHaximus\Travis\Resource\Async;
 
 use WyriHaximus\Travis\Resource\Cache as BaseCache;
+use function React\Promise\reject;
+use function React\Promise\resolve;
 
 class Cache extends BaseCache
 {
     public function refresh() : Cache
     {
-        return $this->wait($this->callAsync('refresh'));
+        return $this->getTransport()->request('repos/' . $this->repositoryId() . '/caches')->then(function ($json) {
+            foreach ($json['caches'] as $cache) {
+                if ($cache['slug'] != $this->slug()) {
+                    continue;
+                }
+
+                return resolve($this->getTransport()->getHydrator()->hydrate('Cache', $cache));
+            }
+
+            return reject();
+        });
     }
 }
