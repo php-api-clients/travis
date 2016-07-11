90f8a2b8645b8e4b01e54ed4e3a1b60fad6e8e52
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-07-11 22:11:54 +0200

    SSH key refresh

--- src/Resource/Async/SSHKey.php
+++ src/Resource/Async/SSHKey.php
@@ -3,12 +3,19 @@ declare(strict_types=1);
 
 namespace WyriHaximus\Travis\Resource\Async;
 
+use React\Promise\PromiseInterface;
 use WyriHaximus\Travis\Resource\SSHKey as BaseSSHKey;
+use function React\Promise\resolve;
 
 class SSHKey extends BaseSSHKey
 {
-    public function refresh() : SSHKey
+    /**
+     * @return PromiseInterface
+     */
+    public function refresh() : PromiseInterface
     {
-        return $this->wait($this->callAsync('refresh'));
+        return $this->getTransport()->request('settings/ssh_key/' . $this->id())->then(function ($json) {
+            return resolve($this->getTransport()->getHydrator()->hydrate('SSHKey', $json['ssh_key']));
+        });
     }
 }
