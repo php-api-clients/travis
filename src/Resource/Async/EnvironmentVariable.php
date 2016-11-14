f504e8b49adb0f3701a5ddefa29cc01b064600a3
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-11-14 22:11:40 +0100

    EnvironmentVariable refresh

--- src/Resource/Async/EnvironmentVariable.php
+++ src/Resource/Async/EnvironmentVariable.php
@@ -3,12 +3,25 @@ declare(strict_types=1);
 
 namespace WyriHaximus\Travis\Resource\Async;
 
+use React\Promise\PromiseInterface;
+use function React\Promise\reject;
+use function React\Promise\resolve;
 use WyriHaximus\Travis\Resource\EnvironmentVariable as BaseEnvironmentVariable;
 
 class EnvironmentVariable extends BaseEnvironmentVariable
 {
-    public function refresh() : EnvironmentVariable
+    public function refresh() : PromiseInterface
     {
-        return $this->wait($this->callAsync('refresh'));
+        return $this->getTransport()->request('/settings/env_vars?repository_id=' . $this->repositoryId())->then(function ($json) {
+            foreach ($json['env_vars'] as $envVar) {
+                if ($envVar['id'] != $this->id()) {
+                    continue;
+                }
+
+                return resolve($this->getTransport()->getHydrator()->hydrate('EnvironmentVariable', $envVar));
+            }
+
+            return reject();
+        });
     }
 }
