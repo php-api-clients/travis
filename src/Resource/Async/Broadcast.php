95ad7595b00352e0a155b637b89e98841dd03b25
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-11-14 17:49:26 +0100

    Broadcast refresh

--- src/Resource/Async/Broadcast.php
+++ src/Resource/Async/Broadcast.php
@@ -1,16 +1,26 @@
-<?php
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace WyriHaximus\Travis\Resource\Async;
 
 use React\Promise\PromiseInterface;
 use WyriHaximus\Travis\Resource\Broadcast as BaseBroadcast;
+use function React\Promise\reject;
 use function React\Promise\resolve;
 
 class Broadcast extends BaseBroadcast
 {
     public function refresh() : PromiseInterface
     {
-        return resolve($this);
+        return $this->getTransport()->request('repos/' . $this->repositoryId() . '/branches')->then(function ($json) {
+            foreach ($json['broadcasts'] as $broadcast) {
+                if ($broadcast['id'] != $this->id()) {
+                    continue;
+                }
+
+                return resolve($this->getTransport()->getHydrator()->hydrate('Broadcast', $broadcast));
+            }
+
+            return reject();
+        });
     }
 }
