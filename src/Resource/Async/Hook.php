145db0f12c62ddf7c50ad1e9553e92f0e6da9f76
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-11-14 22:00:07 +0100

    Hook refresh

--- src/Resource/Async/Hook.php
+++ src/Resource/Async/Hook.php
@@ -3,12 +3,24 @@ declare(strict_types=1);
 
 namespace WyriHaximus\Travis\Resource\Async;
 
+use function React\Promise\reject;
+use function React\Promise\resolve;
 use WyriHaximus\Travis\Resource\Hook as BaseHook;
 
 class Hook extends BaseHook
 {
     public function refresh() : Hook
     {
-        return $this->wait($this->callAsync('refresh'));
+        return $this->getTransport()->request('hooks')->then(function ($json) {
+            foreach ($json['hooks'] as $hook) {
+                if ($hook['id'] != $this->id()) {
+                    continue;
+                }
+
+                return resolve($this->getTransport()->getHydrator()->hydrate('Hook', $hook));
+            }
+
+            return reject();
+        });
     }
 }
