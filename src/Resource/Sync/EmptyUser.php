ac7dc2e4c51910aa94762daa4d65d144d12bb458
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-11-25 17:58:35 +0100

    Sync empty resources

---
+++ src/Resource/Sync/EmptyUser.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\Travis\Resource\Sync;
+
+use WyriHaximus\Travis\Resource\EmptyUser as BaseEmptyUser;
+
+class EmptyUser extends BaseEmptyUser
+{
+}
