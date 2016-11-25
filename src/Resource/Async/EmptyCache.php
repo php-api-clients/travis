f60f6b664a84317d7a89c1137a7a1e019b1c0b68
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-11-25 17:58:21 +0100

    Async empty resources

---
+++ src/Resource/Async/EmptyCache.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\Travis\Resource\Async;
+
+use WyriHaximus\Travis\Resource\EmptyCache as BaseEmptyCache;
+
+class EmptyCache extends BaseEmptyCache
+{
+}
