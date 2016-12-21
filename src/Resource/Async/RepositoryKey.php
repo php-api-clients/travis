e1551933db148f58aa7088c15444505ad6bb39d2
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-12-21 08:09:54 +0100

    Let the RepositoryKey return itself on refresh as we currently have no means to really refresh it

--- src/Resource/Async/RepositoryKey.php
+++ src/Resource/Async/RepositoryKey.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 namespace ApiClients\Client\Travis\Resource\Async;
 
 use ApiClients\Client\Travis\Resource\RepositoryKey as BaseRepositoryKey;
+use React\Promise\PromiseInterface;
+use function React\Promise\resolve;
 
 class RepositoryKey extends BaseRepositoryKey
 {
-    public function refresh() : RepositoryKey
+    public function refresh() : PromiseInterface
     {
-        return $this->wait($this->callAsync('refresh'));
+        return resolve($this);
     }
 }
