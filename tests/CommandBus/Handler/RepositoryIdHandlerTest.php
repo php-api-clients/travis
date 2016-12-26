36451083edec17aeb3e70d998ccaf0d2b747ec79
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-12-26 19:06:08 +0100

    Fixed repo id handler test

--- tests/CommandBus/Handler/RepositoryIdHandlerTest.php
+++ tests/CommandBus/Handler/RepositoryIdHandlerTest.php
@@ -3,20 +3,11 @@
 namespace ApiClients\Tests\Client\Travis\CommandBus\Handler;
 
 use ApiClients\Client\Travis\CommandBus\Command\RepositoryIdCommand;
-use ApiClients\Client\Travis\CommandBus\Command\SSHKeyCommand;
 use ApiClients\Client\Travis\CommandBus\Handler\RepositoryIdHandler;
-use ApiClients\Client\Travis\CommandBus\Handler\SSHKeyHandler;
-use ApiClients\Client\Travis\Resource\SSHKeyInterface;
+use ApiClients\Client\Travis\Resource\RepositoryInterface;
 use ApiClients\Tools\Services\Client\FetchAndHydrateService;
-use ApiClients\Foundation\Hydrator\Hydrator;
-use ApiClients\Foundation\Transport\ClientInterface;
-use ApiClients\Foundation\Transport\JsonStream;
-use ApiClients\Foundation\Transport\Service\RequestService;
 use ApiClients\Tools\TestUtilities\TestCase;
 use Prophecy\Argument;
-use Psr\Http\Message\RequestInterface;
-use React\EventLoop\Factory;
-use RingCentral\Psr7\Response;
 use function Clue\React\Block\await;
 use function React\Promise\resolve;
 
@@ -26,7 +17,7 @@ final class RepositoryIdHandlerTest extends TestCase
     {
         $command = new RepositoryIdCommand(123);
         $service = $this->prophesize(FetchAndHydrateService::class);
-        $service->handle('jobs/123', 'job', JobInterface::HYDRATE_CLASS)->shouldBeCalled();
+        $service->handle('repos/123', 'repo', RepositoryInterface::HYDRATE_CLASS)->shouldBeCalled();
         $handler = new RepositoryIdHandler($service->reveal());
         $handler->handle($command);
     }
