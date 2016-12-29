fd8d5340d356ca894b16764dea13c3167ab65e8e
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-12-29 17:27:50 +0100

    Client::createFromClient test

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -2,12 +2,14 @@
 
 namespace ApiClients\Tests\Client\Travis;
 
+use ApiClients\Client\Travis\AsyncClientInterface;
 use ApiClients\Client\Travis\Client;
 use ApiClients\Client\Travis\CommandBus\Command;
 use ApiClients\Client\Travis\ClientInterface;
 use ApiClients\Tools\TestUtilities\TestCase;
 use Prophecy\Argument;
 use function Clue\React\Block\await;
+use React\EventLoop\Factory;
 use function React\Promise\resolve;
 
 final class ClientTest extends TestCase
@@ -15,6 +17,14 @@ final class ClientTest extends TestCase
     public function testCreate()
     {
         $client = Client::create();
+
+        self::assertInstanceOf(ClientInterface::class, $client);
+        self::assertInstanceOf(Client::class, $client);
+    }
+
+    public function testCreateFromClient()
+    {
+        $client = Client::createFromClient(Factory::create(), $this->prophesize(AsyncClientInterface::class)->reveal());
 
         self::assertInstanceOf(ClientInterface::class, $client);
         self::assertInstanceOf(Client::class, $client);
