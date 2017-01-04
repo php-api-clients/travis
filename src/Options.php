9fcef08db65500bbf23bd57a587e5da1d043848e
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-01-04 20:59:13 +0100

    Documented constants in the Options class

--- src/Options.php
+++ src/Options.php
@@ -4,5 +4,9 @@ namespace ApiClients\Client\Travis;
 
 final class Options
 {
+    /**
+     * The authentication token from travis,
+     * for more information how to retrieve it: https://blog.travis-ci.com/2013-01-28-token-token-token/
+     */
     const TOKEN = 'token';
 }
