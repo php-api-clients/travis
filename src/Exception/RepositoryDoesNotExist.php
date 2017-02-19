073af737dd19484a066728f21b314277a86a0549
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-02-19 13:10:54 +0100

    Repository does not exist exception

---
+++ src/Exception/RepositoryDoesNotExist.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Client\Travis\Exception;
+
+use Exception;
+
+final class RepositoryDoesNotExist extends Exception
+{
+    /**
+     * @var string
+     */
+    private $repository;
+
+    public static function create(string $repository)
+    {
+        $exception = new self('Repository "' . $repository . '" does not exist');
+        $exception->repository = $repository;
+        return $exception;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRepository(): string
+    {
+        return $this->repository;
+    }
+}
